1705ff295e53c84b247351b40315aa2c71d8d61b
Steve Malers <steve.malers@openwaterfoundation.org>
2018-06-12 14:12:30 -0600

    Reviewed Kristins updates

--- build-util/copy-to-owf-amazon-s3.sh
+++ build-util/copy-to-owf-amazon-s3.sh
@@ -7,7 +7,7 @@
 # Set --dryrun to test before actually doing
 dryrun=""
 #dryrun="--dryrun"
-s3Folder="s3://stories.openwaterfoundation.org/co/swsi-story-ipps"
+s3Folder="s3://stories.openwaterfoundation.org/co/swsi-story-ipps-sp"
 
 # Make sure that this is being run from the build-util folder
 pwd=`pwd`
@@ -30,4 +30,33 @@ fi
 
 awsProfile="$1"
 
-aws s3 sync ../site-basic-example ${s3Folder} ${dryrun} --delete --profile "$awsProfile"
+# Get the version to append to files
+versionFile="../site/VERSION.txt"
+version=`cat "${versionFile}"`
+if [ -z "${version}" ]
+        then
+        echo ""
+        echo "No ${versionFile} version file to modify filenames for caching- cannot continue."
+        exit 1
+fi
+
+# Folder used for temporary files, mainly renamed files to prevent caching
+tmpBuildFolder="tmp-build"
+if [ ! -e "${tmpBuildFolder}" ]
+        then
+        echo ""
+        echo "No temporary build folder ${tmpBuildFolder} - cannot continue."
+        exit 1
+fi
+
+# Sync first, then copy specific files
+aws s3 sync ../site ${s3Folder} ${dryrun} --delete --profile "$awsProfile"
+# Update content of index.html to use versioned files
+cssOrig='style.css'
+cssWithVersion="style.${version}.css"
+customleafletcssOrig='custom-leaflet-style.css'
+customleafletcssWithVersion="custom-leaflet-style.${version}.css"
+cat ../site/index.html | sed -e "s/${cssOrig}/${cssWithVersion}/g" | sed -e "s/${customleafletcssOrig}/${customleafletcssWithVersion}/g" > ${tmpBuildFolder}/index.html
+aws s3 cp ${tmpBuildFolder}/index.html ${s3Folder}/index.html ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/css/style.css ${s3Folder}/css/style.${version}.css ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/css/custom-leaflet-style.css ${s3Folder}/css/custom-leaflet-style.${version}.css ${dryrun} --profile "$awsProfile"
