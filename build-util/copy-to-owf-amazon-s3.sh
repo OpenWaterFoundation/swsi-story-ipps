5adf0020c784f71d76cf2e15b14e6fce42a6c8d4
Steve Malers <steve.malers@openwaterfoundation.org>
2018-07-25 17:31:02 -0600

    Update uploader script for current file list

--- build-util/copy-to-owf-amazon-s3.sh
+++ build-util/copy-to-owf-amazon-s3.sh
@@ -52,11 +52,34 @@ fi
 # Sync first, then copy specific files
 aws s3 sync ../site ${s3Folder} ${dryrun} --delete --profile "$awsProfile"
 # Update content of index.html to use versioned files
-cssOrig='style.css'
-cssWithVersion="style.${version}.css"
+# css/
 customleafletcssOrig='custom-leaflet-style.css'
 customleafletcssWithVersion="custom-leaflet-style.${version}.css"
-cat ../site/index.html | sed -e "s/${cssOrig}/${cssWithVersion}/g" | sed -e "s/${customleafletcssOrig}/${customleafletcssWithVersion}/g" > ${tmpBuildFolder}/index.html
+mapCssOrig='map.css'
+mapCssWithVersion="map.${version}.css"
+styleCssOrig='style.css'
+styleCssWithVersion="style.${version}.css"
+# js/map-files/
+exampleIppMap1JsOrig='example-ipp-map-1.js'
+exampleIppMap1JsWithVersion="example-ipp-map-1.${version}.js"
+exampleIppMap2JsOrig='example-ipp-map-2.js'
+exampleIppMap2JsWithVersion="example-ipp-map-2.${version}.js"
+ippMap1JsOrig='ipp-map-1.js'
+ippMap1JsWithVersion="ipp-map-1.${version}.js"
+ippMap2JsOrig='ipp-map-2.js'
+ippMap2JsWithVersion="ipp-map-2.${version}.js"
+ippMap3JsOrig='ipp-map-3.js'
+ippMap3JsWithVersion="ipp-map-3.${version}.js"
+isfMapJsOrig='isf-map.js'
+isfMapJsWithVersion="isf-map.${version}.js"
+cat ../site/index.html | sed -e "s/${customleafletcssOrig}/${customleafletcssWithVersion}/g" | sed -e "s/$mapCssOrig}/${mapCssWithVersion}/g" | sed -e "s/${styleCssOrig}/${styleCssWithVersion}/g" | sed -e "s/${exampleIppMap1JsOrig}/${exampleIppMap1JsWithVersion}/g" | sed -e "s/${exampleIppMap2JsOrig}/${exampleIppMap2JsWithVersion}/g" | sed -e "s/${ippMap1JsOrig}/${ippMap1JsWithVersion}/g" | sed -e "s/${ippMap2JsOrig}/${ippMap2JsWithVersion}/g" | sed -e "s/${ippMap3JsOrig}/${ippMap3JsWithVersion}/g" | sed -e "s/${isfMapJsOrig}/${isfMapJsWithVersion}/g" > ${tmpBuildFolder}/index.html
 aws s3 cp ${tmpBuildFolder}/index.html ${s3Folder}/index.html ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/css/style.css ${s3Folder}/css/style.${version}.css ${dryrun} --profile "$awsProfile"
 aws s3 cp ../site/css/custom-leaflet-style.css ${s3Folder}/css/custom-leaflet-style.${version}.css ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/css/map.css ${s3Folder}/css/map.${version}.css ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/css/style.css ${s3Folder}/css/style.${version}.css ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/js/map-files/example-ipp-map-1.js ${s3Folder}/js/map-files/example-ipp-map-1.${version}.js ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/js/map-files/example-ipp-map-2.js ${s3Folder}/js/map-files/example-ipp-map-2.${version}.js ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/js/map-files/ipp-map-1.js ${s3Folder}/js/map-files/ipp-map-1.${version}.js ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/js/map-files/ipp-map-2.js ${s3Folder}/js/map-files/ipp-map-2.${version}.js ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/js/map-files/ipp-map-3.js ${s3Folder}/js/map-files/ipp-map-3.${version}.js ${dryrun} --profile "$awsProfile"
+aws s3 cp ../site/js/map-files/isf-map.js ${s3Folder}/js/map-files/isf-map.${version}.js ${dryrun} --profile "$awsProfile"
