40691fcf8b455df2e399e0c1875b624eed914a0d
jurentie <jurentie@rams.colostate.edu>
2018-09-13 11:12:04 -0600

    updated script to allow creating files without upload for uploading site files to southplattebasin.com

--- build-util/copy-to-owf-amazon-s3.sh
+++ build-util/copy-to-owf-amazon-s3.sh
@@ -8,6 +8,12 @@
 dryrun=""
 #dryrun="--dryrun"
 s3Folder="s3://stories.openwaterfoundation.org/co/swsi-story-ipps-sp"
+# runMode indicates what the script should do
+# - upload = prep and upload to Amazon S3,
+#   used when uploading to OWF website
+# - prepUpload = prep the site for upload but don't do it,
+#   used when creating the site folder for southplattebasin.com
+runMode="upload"
 
 # Make sure that this is being run from the build-util folder
 pwd=`pwd`
@@ -26,6 +32,14 @@ if [ "$1" == "" ]
 	echo "Copy the site files to the Amazon S3 static website folder:  $s3Folder"
 	echo ""
 	exit 0
+fi
+
+if [ "$1" == "prep-upload" ]
+        then
+        # Prep the website for upload in the tmp-build folder but do not actually do the upload.
+        # - the site can then be uploaded to southplattebasin.com
+        # - don't actually do the upload
+        runMode="prepUpload"
 fi
 
 awsProfile="$1"
@@ -49,8 +63,14 @@ if [ ! -e "${tmpBuildFolder}" ]
         exit 1
 fi
 
-# Sync first, then copy specific files
-aws s3 sync ../site ${s3Folder} ${dryrun} --delete --profile "$awsProfile"
+# First clean out the contents of the "tmp-build" folder
+echo "Remove files from ${tmpBuildFolder}"
+rm -rf ${tmpBuildFolder}/*
+
+# Copy the entire site into the "tmp-build" folder
+echo "Copy ../site to ${tmpBuildFolder}"
+cp -r ../site/* ${tmpBuildFolder}
+
 # Update content of index.html to use versioned files
 # css/
 customleafletcssOrig='custom-leaflet-style.css'
@@ -72,14 +92,35 @@ ippMap3JsOrig='ipp-map-3.js'
 ippMap3JsWithVersion="ipp-map-3.${version}.js"
 isfMapJsOrig='isf-map.js'
 isfMapJsWithVersion="isf-map.${version}.js"
-cat ../site/index.html | sed -e "s/${customleafletcssOrig}/${customleafletcssWithVersion}/g" | sed -e "s/$mapCssOrig}/${mapCssWithVersion}/g" | sed -e "s/${styleCssOrig}/${styleCssWithVersion}/g" | sed -e "s/${exampleIppMap1JsOrig}/${exampleIppMap1JsWithVersion}/g" | sed -e "s/${exampleIppMap2JsOrig}/${exampleIppMap2JsWithVersion}/g" | sed -e "s/${ippMap1JsOrig}/${ippMap1JsWithVersion}/g" | sed -e "s/${ippMap2JsOrig}/${ippMap2JsWithVersion}/g" | sed -e "s/${ippMap3JsOrig}/${ippMap3JsWithVersion}/g" | sed -e "s/${isfMapJsOrig}/${isfMapJsWithVersion}/g" > ${tmpBuildFolder}/index.html
-aws s3 cp ${tmpBuildFolder}/index.html ${s3Folder}/index.html ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/css/custom-leaflet-style.css ${s3Folder}/css/custom-leaflet-style.${version}.css ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/css/map.css ${s3Folder}/css/map.${version}.css ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/css/style.css ${s3Folder}/css/style.${version}.css ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/js/map-files/example-ipp-map-1.js ${s3Folder}/js/map-files/example-ipp-map-1.${version}.js ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/js/map-files/example-ipp-map-2.js ${s3Folder}/js/map-files/example-ipp-map-2.${version}.js ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/js/map-files/ipp-map-1.js ${s3Folder}/js/map-files/ipp-map-1.${version}.js ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/js/map-files/ipp-map-2.js ${s3Folder}/js/map-files/ipp-map-2.${version}.js ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/js/map-files/ipp-map-3.js ${s3Folder}/js/map-files/ipp-map-3.${version}.js ${dryrun} --profile "$awsProfile"
-aws s3 cp ../site/js/map-files/isf-map.js ${s3Folder}/js/map-files/isf-map.${version}.js ${dryrun} --profile "$awsProfile"
+#
+# Replace references in index.html with files that have versions
+echo "Update index.html with versioned references"
+cat ../site/index.html | sed -e "s/${customleafletcssOrig}/${customleafletcssWithVersion}/g" | sed -e "s/${mapCssOrig}/${mapCssWithVersion}/g" | sed -e "s/${styleCssOrig}/${styleCssWithVersion}/g" | sed -e "s/${exampleIppMap1JsOrig}/${exampleIppMap1JsWithVersion}/g" | sed -e "s/${exampleIppMap2JsOrig}/${exampleIppMap2JsWithVersion}/g" | sed -e "s/${ippMap1JsOrig}/${ippMap1JsWithVersion}/g" | sed -e "s/${ippMap2JsOrig}/${ippMap2JsWithVersion}/g" | sed -e "s/${ippMap3JsOrig}/${ippMap3JsWithVersion}/g" | sed -e "s/${isfMapJsOrig}/${isfMapJsWithVersion}/g" > ${tmpBuildFolder}/index.html
+
+#Copy the original files and add version to the filename
+echo "Copy versioned files to ${tmpBuildFolder}"
+
+cp ../site/css/custom-leaflet-style.css ${tmpBuildFolder}/css/custom-leaflet-style.${version}.css
+cp ../site/css/map.css ${tmpBuildFolder}/css/map.${version}.css
+cp ../site/css/style.css ${tmpBuildFolder}/css/style.${version}.css
+
+cp ../site/js/map-files/example-ipp-map-1.js ${tmpBuildFolder}/js/map-files/example-ipp-map-1.${version}.js
+cp ../site/js/map-files/example-ipp-map-2.js ${tmpBuildFolder}/js/map-files/example-ipp-map-2.${version}.js
+cp ../site/js/map-files/ipp-map-1.js ${tmpBuildFolder}/js/map-files/ipp-map-1.${version}.js
+cp ../site/js/map-files/ipp-map-2.js ${tmpBuildFolder}/js/map-files/ipp-map-2.${version}.js
+cp ../site/js/map-files/ipp-map-3.js ${tmpBuildFolder}/js/map-files/ipp-map-3.${version}.js
+cp ../site/js/map-files/isf-map.js ${tmpBuildFolder}/js/map-files/isf-map.${version}.js
+
+# Create zip folder if runMode = prepUpload
+if [ "$runMode" == "prepUpload" ]
+        then
+        # Zip files using 7zip
+        echo "Zip site files"
+        7z a -tzip swsi-story-ipps.zip ${tmpBuildFolder}/*
+fi
+
+if [ "$runMode" == "upload" ]
+        then
+        # Sync the tmp-build folder to Amazon S3
+        aws s3 sync ${tmpBuildFolder} ${s3Folder} ${dryrun} --delete --profile "$awsProfile"
+fi
