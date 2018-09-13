1b7fc1134904aec80b277e98c9e75a354e5fa8c7
jurentie <jurentie@rams.colostate.edu>
2018-09-13 11:12:35 -0600

    will serve tmp-build folder if passed ANY command line parameter

--- build-util/run-http-server-8000.sh
+++ build-util/run-http-server-8000.sh
@@ -69,7 +69,16 @@ runWebServer () {
 # Main entry point into script
 
 # Change to the website files
-cd ../site
-# Run the web server
+if [ "$1" == "" ]
+        then
+        # Development files are in the ../site folder
+        echo "Serving development site files"
+        cd ../site
+else
+        # Files ready for upload
+        echo "Serving packaged upload site files"
+        cd tmp-build
+fi
+
 runWebServer
 exit 0
